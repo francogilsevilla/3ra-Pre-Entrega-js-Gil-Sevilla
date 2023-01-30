123c4dce3281952125a73117edcdd18930747371
Franco Gil <francogil15@gmail.com>
2023-01-30 14:56:35 -0300

    Ajustes

--- js/main.js
+++ js/main.js
@@ -1,64 +1,64 @@
-const clientes_para_avisar = []
-
-const lista_de_productos = [
+const productos = [
     {
         id: 1,
         nombre: 'Gabinete ROG',
-        precio: 20000,
+        precio: 20000
     },
     {
         id: 2,
         nombre: 'Gabinete Redragon',
-        precio: 15000,
+        precio: 15000
     },
     {
         id: 3,
         nombre:'Placa Gtx 1660 ti',
-        precio: 120000,
+        precio: 120000
     },
     {
         id: 4,
         nombre:'Placa Aourus 2090 8gb',
-        precio: 220000,
+        precio: 220000
     },
     {
         id: 5,
         nombre:'Intel Core I5',
-        precio: 35000,
+        precio: 35000
     },
     {
         id: 6,
         nombre:'Intel core I3',
-        precio: 60000,
+        precio: 60000
     },
     {
         id: 7,
         nombre:'Placa Madre Asrock',
-        precio: 15000,
+        precio: 15000
     },
     {
         id: 8,
         nombre:'Placa Madre Gigabyte',
-        precio: 10000,
+        precio: 10000
     },
     {
         id: 9,
         nombre:'RAM Fury 8gb',
-        precio: 24000,
+        precio: 24000
     },
     {
         id: 10,
         nombre:'Ram Crucial 4gb',
-        precio: 12000,
+        precio: 12000
     },
     {
         id: 11,
         nombre:'Fuente Gigabyte 1000w 80plus',
-        precio: 12000,
+        precio: 12000
     },
     {
         id: 12,
         nombre:'Fuente 750w 80 plus',
-        precio: 5000,
-    },
+        precio: 5000
+    }
 ];
+
+let carrito = []
